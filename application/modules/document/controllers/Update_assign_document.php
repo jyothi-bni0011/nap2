cfd71878a3283e46c9abc9ec785b16ceb3aa2ca2
jyothi <jyothi.m@bluenettech.com>
2019-09-09 18:49:07 +0530

    assign document mail issue'

--- application/modules/document/controllers/Update_assign_document.php
+++ application/modules/document/controllers/Update_assign_document.php
@@ -26,7 +26,7 @@ class Update_assign_document extends MY_Controller {
             if (array_key_exists('documents', $_POST)) {
                 /* Insert document mapping for new associate */
                 if ($this->assign_document_model->update($this->input->post('documents'), $associate_id)) {
-                    $send_mail=$this->create_model->send_email( 'reassign_documents',$values, $values );
+                    $send_mail=$this->create_model->send_email( 'reassign_documents',$values->email, $values );
                    $this->session->set_flashdata('message', 'New Associate has been updated.');
                    redirect('/new_associate', $data);
                 }else{
